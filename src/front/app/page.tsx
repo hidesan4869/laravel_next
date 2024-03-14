c2e01f3ae71fe2fd2bf25af29785a92cfc3bb80d
鳥居英矢 <h.torii@piary.jp>
2024-03-14 20:12:18 +0900

    init

--- src/front/app/page.tsx
+++ src/front/app/page.tsx
@@ -1,3 +1,54 @@
+'use client';
+
+let result: string = 'none';
+
 export default function Home() {
-    return <h1>Hello World</h1>;
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24">
+            test
+            <TestButton />
+        </main>
+    )
+}
+
+const TestButton = () => {
+    return <button className='test-button' onClick={ GetRequest } data-url='http://localhost:13000/api/test'>GET</button>
+}
+
+async function GetRequest(e: React.MouseEvent) {
+    const targetURL = e.currentTarget.getAttribute('data-url');
+    if(targetURL == null || targetURL == '')
+        alert('URLError!');
+    else{
+        await fetch(targetURL, {
+            method: 'GET'
+        }).then(
+            response => {
+                if (response.status != 200) {
+                    alert('エラー');
+                } else {
+                    return response.json();
+                }
+            }
+        )
+        .then((jsonData) => {
+            if (jsonData != null) {
+                const status = jsonData['status'];
+                if ( status == 'ok') {
+                    result = status;
+                } else {
+                    result = 'ステータスエラー';
+                }
+            } else {
+                result = 'レスポンスエラー';
+            }
+        })
+        .catch(error => {
+            result = 'catch!';
+        });
+
+        let resultField = document.getElementById('result-field');
+        if(resultField)
+        resultField.innerText = result;
+    }
 }
